cfeb1c2667b8e481b4ae95c0e23fa206208d238d
Luiz-nascimento012 <nasciluix052@gmail.com>
2025-12-27 19:00:01 -0300

    mais desenvolvimento

---
+++ testes/main.js
@@ -0,0 +1,9 @@
+var n1 = parseFloat(window.prompt("digite um número"))
+
+var n2 = parseFloat(window.prompt("digite outro número"))
+
+var resto = n1 % n2
+
+window.alert(`o resultado dessa merda é ${resto}`)
+
+
